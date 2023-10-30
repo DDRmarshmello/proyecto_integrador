8924a13a64b6a29c3878c70239fae8945cf11e79
DDRmarshmello <100654765+DDRmarshmello@users.noreply.github.com>
2023-10-30 09:36:50 -0400

    Update order.py

--- routes/order.py
+++ routes/order.py
@@ -1,22 +1,23 @@
 from fastapi import APIRouter
 from config.db import conn
-from models.models import orders, details_order
-from schemas.Schemas import Order, OrderStatus, Details
-from sqlalchemy import select, join
 
 order = APIRouter()
 ORDER_URL = '/orders'
+ORDER_STATUS_URL = ORDER_URL + '/status'
 
-@order.get(ORDER_URL)
-async def get_order():
-    r = select(orders.join(details_order, orders.c.id == details_order.c.id_order))
-    return conn.execute(r).fetchall()
- 
+@order.get(ORDER_URL + "/{id}")
+async def get_order(id : int):
+    od = conn.execute("SELECT OD.ID, OD.order_date, OD.t_price, DELI.status, DELI.type FROM proyecto_integrador.ORDER as OD INNER JOIN delivery AS DELI ON OD.delivery_id = DELI.ID INNER JOIN customer as client on OD.customer_id = client.id where id =" + id).fetchall()
+    detail_od = conn.execute("SELECT PROD.product_name, DO.amount_product CATE.category_name DO.t_price, FROM details_order AS DO INNER JOIN products AS PROD ON DO.id_product = PROD.ID INNER JOIN categories AS CATE on PROD.category_id = CATE.ID WHERE DO.id_order =" + id).fetchall()
+    return {"oder": od, "product_details": detail_od} 
+
+@order.get(ORDER_STATUS_URL)
+async def orders_status():
+    order_status = conn.execute("SELECT OD.ID, OD.order_date, OD.t_price, DELI.status, DELI.type FROM proyecto_integrador.ORDER as OD INNER JOIN delivery AS DELI ON OD.delivery_id = DELI.ID INNER JOIN customer as client on OD.customer_id = client.id").fetchall()
+    return order_status
+
+@order.get(ORDER_STATUS_URL + "/{id}")
+async def orders_status(id : int):
+    order_status = conn.execute("SELECT OD.ID, OD.order_date, OD.t_price, DELI.status, DELI.type FROM proyecto_integrador.ORDER as OD INNER JOIN delivery AS DELI ON OD.delivery_id = DELI.ID INNER JOIN customer as client on OD.customer_id = client.id=" + id).fetchall()
+    return order_status
   
-
-    
-""" from sqlalchemy import join
-
-j = user_table.join(address_table,
-                user_table.c.id == address_table.c.user_id)
-stmt = select(user_table).select_from(j) """
